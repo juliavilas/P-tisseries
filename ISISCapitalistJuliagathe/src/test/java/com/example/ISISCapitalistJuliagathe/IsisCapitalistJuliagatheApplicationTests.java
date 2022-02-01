3c86389824258f8d28e3ec8fe99e976a5f185183
Asutarik <AgatheSk>
2022-02-01 09:03:33 +0100

    Ajout serveur

---
+++ ISISCapitalistJuliagathe/src/test/java/com/example/ISISCapitalistJuliagathe/IsisCapitalistJuliagatheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISISCapitalistJuliagathe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsisCapitalistJuliagatheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
