de37842cbc406374cdd5b1bba40f2480f0e3d977
julia.vilas@etud.univ-jfc.fr <julia.vilas@etud.univ-jfc.fr>
2022-02-25 15:14:10 +0100

    Progression Product.tsx

--- adventure_shrek/src/Product.tsx
+++ adventure_shrek/src/Product.tsx
@@ -22,15 +22,20 @@ export default function ProductComponent({ prod, services }: ProductProps) {
     //     }
     // }, [])
     return (
-            <div className="product">
-                <div className="round"><img src={services.server + prod.logo}/> </div>
-                  {/* <Box sx={{ width: '100%' }}>
-                        <ProgressBar transitionDuration={"0.1s"} customLabel={" "}
-                            completed={progress} />
-                    </Box> */}
-              <span>{prod.name}</span><br/>
-              <span>{prod.cout}</span><br/>
-              <span> {prod.croissance}</span>
-              </div>
+        <div className="product">
+            <span>{prod.name}</span><br />
+            <div className="grid">
+                <div className="imageProduit"><img src={services.server + prod.logo} /></div>
+
+                <div>Box{/* <Box sx={{ width: '100%' }}>
+                          <ProgressBar transitionDuration={"0.1s"} customLabel={" "}
+                              completed={progress} />
+                      </Box>  */}</div>
+                <div><span>Quantité : {prod.quantite}</span><br /></div>
+                <div>bouton acheter</div>
+                <div>temps fabrication</div>
+                {/* <span>Revenu : {prod.revenu}</span> */}
+            </div>
+        </div>
     )
 }
