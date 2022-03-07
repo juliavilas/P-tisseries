b01133024c69170dffcb113df3588b377c7a0e9b
AgatheSk <ehtaga.kiratus@gmail.com>
2022-03-07 18:44:50 +0100

    code pour la barre de progression qui remarche

--- adventure_shrek/src/Product.tsx
+++ adventure_shrek/src/Product.tsx
@@ -25,11 +25,12 @@ type ProductProps = {
 
 export default function ProductComponent({prod,/*onProductionDone,*/ services}: ProductProps) {
     const [progress, setProgress] = useState(0);
-    let progressbarvalue=0;
+    let progressbarvalue : number
     function startFabrication() {
         prod.timeleft=prod.vitesse;
         prod.lastupdate=Date.now();
         console.log("click");
+        progressbarvalue=0;
     }
 
     const savedCallback = useRef(calcScore);
@@ -48,21 +49,26 @@ export default function ProductComponent({prod,/*onProductionDone,*/ services}: 
  }*/
 
     function calcScore(){
-        let tpsEcoule=0;
+        let tpsEcoule:number;
         if (prod.timeleft != 0) {
             tpsEcoule=Math.floor((Date.now()-prod.lastupdate)/1000)
             prod.timeleft -= tpsEcoule;
-            if (prod.timeleft <= 0)
+            console.log(prod.timeleft)
+            if (prod.timeleft <= 0){
                 if (prod.timeleft < 0) {
                     //onProductionDone(prod);
                     prod.timeleft = 0;
                     progressbarvalue=0;
-                } else{
-                    progressbarvalue= ((prod.vitesse - prod.timeleft) / prod.vitesse) * 100
-                }
-                setProgress(progressbarvalue);
+                } 
+            }else{
+                    progressbarvalue=((prod.vitesse - prod.timeleft) / prod.vitesse) * 100
+            }
+            setProgress(progressbarvalue);
+            console.log("progress "+progressbarvalue)
+        }else{
+            setProgress(0)
         }
-
+       // prod.quantite++
      }
 
     if (prod == undefined) {
@@ -71,11 +77,12 @@ export default function ProductComponent({prod,/*onProductionDone,*/ services}: 
     } else {
         console.log(prod.name)
     }
+    let achat="Acheter x1   "+prod.cout+"$"
     return (
         <div className="product">
             <span>{prod.name}</span>
             <div className="grid">
-                <div id="image"><img src={services.server + prod.logo} id="imageProduit"/>
+                <div id="image"><a href="#" onClick={startFabrication}><img src={services.server + prod.logo} id="imageProduit"/></a>
                     <div className="composantGrid" id="quantite">{prod.quantite}</div>
                 </div>
 
@@ -84,8 +91,7 @@ export default function ProductComponent({prod,/*onProductionDone,*/ services}: 
                     {<ProgressBar transitionDuration={"0.1s"} customLabel={" "} completed={progress}/>}
                     {/*</Box>*/}
                 </div>
-                <div className="composantGrid"><input type="button" id="boutonAcheter" value="Acheter x1   {prod.cout}"
-                                                      onClick={startFabrication}/></div>
+                <div className="composantGrid"><input type="button" id="boutonAcheter" value={achat} onClick={startFabrication}/></div>
                 <div className="composantGrid">{prod.timeleft} s</div>
                 {/* <span>Revenu : {prod.revenu}</span> */}
             </div>
